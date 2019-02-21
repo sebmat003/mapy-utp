c9db79f9e9998c2dee9a77100f032ce7cbdfc141
sebmat003 <sebmat003@utp.edu.pl>
2019-02-21 10:34:58 +0100

    left-menu show and hide

--- src/app/left-menu/left-menu.component.ts
+++ src/app/left-menu/left-menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftMenuComponent implements OnInit {
 
+  private showLeftMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
