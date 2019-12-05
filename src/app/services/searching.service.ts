bceb8b3f2d9ff737c61db7000d69d427cebba66a
Sebastian Matusiak <sebmat003@utp.edu.pl>
2019-12-05 10:54:41 +0100

    added content to room info panel

--- src/app/services/searching.service.ts
+++ src/app/services/searching.service.ts
@@ -23,6 +23,7 @@ export class SearchingService {
   employeesInUnitData;
   employeeInfoData;
   employeeInfoUrl = 'https://api-dev.kb.utp.edu.pl/search-provider/employee?phrase=';
+  consultations;
   roomAndEmployeeData;
   roomAndEmployeeUrl = 'https://api-dev.kb.utp.edu.pl/search-provider/common/search?phrase=';
   campusData;
@@ -35,13 +36,14 @@ export class SearchingService {
   floorUrl2 = '?sortField=name&sortDirection=ASC';
   roomsUrl = 'https://api-dev.kb.utp.edu.pl/search-provider/room/search/filter?';
   roomsData;
-  roomInfoUrl = 'https://api-dev.kb.utp.edu.pl/search-provider/room/search?phrase=';
-  roomInfoData;
+  roomInfoUrl = 'https://api-dev.kb.utp.edu.pl/search-provider/room?roomId=';
+  roomInfoData = null;
 
 
   //info component about employee/room
   numberOfClicks: number = 0;
   clickedListItem = false;
+
 
   constructor(private httpClient: HttpClient) {
     this.httpClient.get(this.unitsUrl, {responseType: 'json'})
@@ -60,66 +62,76 @@ export class SearchingService {
     this.httpClient.get(this.roomAndEmployeeUrl + text, {responseType: 'json'})
       .subscribe((data) => {
         this.roomAndEmployeeData = data;
-      })
+      });
   }
 
   getEmployeesData(unitId: string | number) {
     this.httpClient.get(this.employeesUrl + unitId, {responseType: 'json'})
-      .subscribe( (data) => {
+      .subscribe((data) => {
         this.employeesInUnitData = data;
-      })
+      });
   }
 
   getEmployeeInfoData(text: string) {
     this.httpClient.get(this.employeeInfoUrl + text, {responseType: 'json'})
-      .subscribe( (data) => {
+      .subscribe((data) => {
         this.employeeInfoData = data['0'];
-      })
+        let cons = data['0'].additionalData['0'].consultations;
+        if (cons !== null) {
+          this.consultations = Object.entries(cons).map((e) => ({[e[0]]: e[1]}));
+        }
+      });
   }
 
   getCampusData() {
     this.httpClient.get(this.campusUrl, {responseType: 'json'})
-      .subscribe( (data) => {
+      .subscribe((data) => {
         this.campusData = data;
-      })
+      });
   }
 
   getBuildingData(id: string | number) {
     this.httpClient.get(this.buildingUrl1 + id + this.buildingUrl2, {responseType: 'json'})
-      .subscribe( (data) => {
+      .subscribe((data) => {
         this.buildingData = data;
-      })
+      });
   }
 
   getFloorData(id: string | number) {
     this.httpClient.get(this.floorUrl1 + id + this.floorUrl2, {responseType: 'json'})
-      .subscribe( (data) => {
+      .subscribe((data) => {
         this.floorData = data;
-      })
+      });
   }
 
   getRoomsData(campusId: string | number, buildingId: string | number, floorId: string | number) {
-    if(campusId !== null && buildingId !== null && floorId !== null) {
+    if (campusId !== null && buildingId !== null && floorId !== null) {
       this.httpClient.get(this.roomsUrl + 'campusId=' + campusId +
         '&buildingId=' + buildingId + '&floorId=' + floorId, {responseType: 'json'})
-        .subscribe( (data) => {
+        .subscribe((data) => {
           this.roomsData = data;
           console.log(this.roomsData);
-        })
-    } else this.roomsData = null;
+        });
+    } else {
+      this.roomsData = null;
+    }
   }
 
   getRoomInfoData(text: string) {
     this.httpClient.get(this.roomInfoUrl + text, {responseType: 'json'})
-      .subscribe( (data) => {
-        this.roomInfoData = data;
-      })
+      .subscribe((data) => {
+        this.roomInfoData = Array.of(data);
+      });
   }
 
   resetData() {
     switch (this.searchingState) {
-      case 1: this.roomAndEmployeeData = null; break;
-      case 2: this.employeesInUnitData = null; break;
+      case 1:
+        this.roomAndEmployeeData = null;
+        break;
+      case 2:
+        this.employeesInUnitData = null;
+        break;
       case 3: {
         this.buildingData = null;
         this.floorData = null;
